8f7576486864e574ea25526cf2a3d572cdf45c02
Arjay Nicdao <a.nicdao@androbo.jp>
2015-04-21 17:51:17 +0800

    #1 - new ads page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  
   get '/signup', :to => 'users#new'
   get '/login', :to => 'sessions#new'
   post 'login', :to => 'sessions#create'
   get '/logout', :to => 'sessions#destroy'
 
-    resources :users
+  resources :users
   resources :sessions
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
